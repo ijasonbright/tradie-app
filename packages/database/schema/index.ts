399cc96d2dad8bea49ebe53587058eb4810d40b1
ijasonbright <jason@taskforce.com.au>
2025-10-19 20:37:17 +1100

    Temporarily disable relations export to fix build

--- packages/database/schema/index.ts
+++ packages/database/schema/index.ts
@@ -6,4 +6,5 @@ export * from './clients'
 export * from './jobs'
 export * from './quotes'
 export * from './invoices'
-export * from './relations'
+// Temporarily disable relations to fix build
+// export * from './relations'
